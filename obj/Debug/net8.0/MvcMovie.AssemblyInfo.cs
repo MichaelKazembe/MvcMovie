b9364fe89658a8a43882dd3c1597403b688f3527
MichaelKazembe <michaelkazembe@gmail.com>
2026-05-21 23:17:12 +0200

    Add new Field

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a8b01f45c79d3101f23cd270cb98ffc74cdb3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b756bc5de2dd6651c43f66b0577fab70304cad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
