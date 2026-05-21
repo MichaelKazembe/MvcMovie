d09e2554fc60e572e795199c813846b3558d9e5f
MichaelKazembe <michaelkazembe@gmail.com>
2026-05-21 20:49:22 +0200

    add view: Implement Welcome action in HelloWorldController and add corresponding view

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbbcb26fab6eb8dd36189e08e69760fc02a6c9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f49575970c9f3bfba7a4449debd48efa4cc0c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
